0a46449efd2174e0279237ae031a2cab16e04183
hhbb111 <moahanaa3@gmail.com>
2025-02-02 00:38:33 +0700

    Add files via upload

--- R1.bat
+++ R1.bat
@@ -58,15 +58,8 @@ adb shell monkey -p com.tencent.ig -c android.intent.category.LAUNCHER 1
 REM الانتظار لبضع ثوانٍ لضمان فتح اللعبة
 timeout /t 10 /nobreak >nul
 
-REM بدء عملية الحقن كل ثانية لمدة دقيقة (60 مرة)
-for /L %%i in (1,1,700) do (
-    adb push C:\Windows\\Fonts\exx1.so /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
-    timeout /t 1 /nobreak >nul
-)
 
-REM تسجيل السجلات
-echo[
-@echo =============================== LOG ================================
-echo[
-adb logcat | findstr MRBOOT
-color F
+    adb push C:\Windows\Fonts\exx1.so /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
+  
+
+
