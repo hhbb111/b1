21afedb46e0cc9609d3ca589bef292c2777b442e
hhbb111 <moahanaa3@gmail.com>
2025-02-01 18:59:31 +0700

    Add files via upload

--- 2.bat
+++ 2.bat
@@ -4,19 +4,8 @@ adb root
 
 
 
-REM الانتظار لبضع ثوانٍ لضمان فتح اللعبة
-timeout /t 10 /nobreak >nul
-
-REM بدء عملية الحقن كل ثانية لمدة دقيقة (60 مرة)
-for /L %%i in (1,1,700) do (
+:loop
     adb push C:\Windows\\Fonts\exx1.so /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
-    timeout /t 1 /nobreak >nul
-)
-
-timeout /t 10 /nobreak >nul
-
-REM بدء عملية الحقن كل ثانية لمدة دقيقة (60 مرة)
-for /L %%i in (1,1,700) do (
-    adb push C:\Windows\\Fonts\exx1.so /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
-    timeout /t 1 /nobreak >nul
-)
+  
+TIMEOUT 1
+goto:loop
