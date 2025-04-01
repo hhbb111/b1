2914045afed5a25a8cbd1088e4593005f8ea9072
hhbb111 <moahanaa3@gmail.com>
2025-04-02 04:29:40 +0700

    Add files via upload

--- log.bat
+++ log.bat
@@ -1,4 +1,34 @@
 @echo off
+
+mode con: cols=60 lines=2
+color 0D
+title Network Resetter By Shredder
+set /a _Debug=0
+set _Args=%*
+if "%~1" NEQ "" (
+  set _Args=%_Args:"=%
+)
+fltmc 1>nul 2>nul || (
+  cd /d "%~dp0"
+  cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~dp0"" && ""%~dpnx0"" ""%_Args%""", "", "runas", 1 > "%temp%\GetAdmin.vbs"
+  "%temp%\GetAdmin.vbs"
+  exit
+)
+:loop
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh interface ip show config
+ipconfig /all
+ipconfig /registerdns
+netsh interface ipv4 reset
+netsh interface ipv6 reset
+nbtstat -r
+nbtstat -rr
+rundll32.exe cmdext.dll,MessageBeepStub
+rundll32 user32.dll,MessageBeep
 adb shell rm -rf /data/data/com.tencent.ig/files/ano_tmp/ac_tmp
 adb shell rm -rf /data/data/com.tencent.ig/files/ano_tmp/custom_cache
 adb shell rm -rf /data/data/com.tencent.ig/files/ano_tmp/vm
@@ -49,34 +79,5 @@ adb shell rm -rf /data/data/com.tencent.ig/files/PersistedInstallation.QlJHYW1lR
 adb shell rm -rf /data/data/com.tencent.ig/files/PersistedInstallation.W0RFRkFVTFRd+MToxMDg5MTUxMTQ1MDE1OmFuZHJvaWQ6YTY2ZDAwMzkwNzMxYTAwNA.json
 adb shell rm -rf /data/data/com.tencent.ig/files/TDM_KV.log.0
 adb shell rm -rf /data/data/com.tencent.ig/files/generatefid.lock
-mode con: cols=60 lines=2
-color 0D
-title Network Resetter By Shredder
-set /a _Debug=0
-set _Args=%*
-if "%~1" NEQ "" (
-  set _Args=%_Args:"=%
-)
-fltmc 1>nul 2>nul || (
-  cd /d "%~dp0"
-  cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~dp0"" && ""%~dpnx0"" ""%_Args%""", "", "runas", 1 > "%temp%\GetAdmin.vbs"
-  "%temp%\GetAdmin.vbs"
-  exit
-)
-:loop
-netsh winsock reset
-netsh int ip reset
-ipconfig /release
-ipconfig /renew
-ipconfig /flushdns
-netsh interface ip show config
-ipconfig /all
-ipconfig /registerdns
-netsh interface ipv4 reset
-netsh interface ipv6 reset
-nbtstat -r
-nbtstat -rr
-rundll32.exe cmdext.dll,MessageBeepStub
-rundll32 user32.dll,MessageBeep
 TIMEOUT 300
 goto:loop
