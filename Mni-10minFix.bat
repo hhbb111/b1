fdc74ba6c7a86ea1d64481a15a88c9fde9117231
hhbb111 <moahanaa3@gmail.com>
2025-04-24 06:19:57 +0700

    Add files via upload

---
+++ Mni-10minFix.bat
@@ -0,0 +1,1023 @@
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+
+
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+
+
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+
+
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+timeout 180
+
+                                Hello %username%!
+color B
+
+
+ipconfig /flushdns
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset all
+nbtstat -r
+nbtstat -rr
+
